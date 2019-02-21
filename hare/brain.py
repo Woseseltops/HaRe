59c425247fede449f83a7d9c4dd93d71f53d86ce
Woseseltops <w.stoop@let.ru.nl>
2019-02-21 16:29:52 +0100

    Added simple dictionary based brain

--- hare/brain.py
+++ hare/brain.py
@@ -27,3 +27,27 @@ class RandomBrain(AbstractBrain):
 
         from random import uniform
         return uniform(0,1)
+
+class DictBasedBrain(AbstractBrain):
+
+    bad_words : List[str] = ['fuck','fck','fuk',
+                             'noob','newb','n00b',
+                             'gay', 'fag',
+                             'shit','stfu',
+                             'suck','loser']
+
+    maximum_nr_of_bad_words_needed : int = 4
+
+    def classify(self,text : str) -> float:
+
+        nr_of_bad_words : int = 0
+
+        for bad_word in self.bad_words:
+            nr_of_bad_words += text.lower().count(bad_word)
+
+        score : float = nr_of_bad_words/self.maximum_nr_of_bad_words_needed
+
+        if score > 1:
+            return 1.0
+        else:
+            return score
